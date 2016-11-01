83e6bf7710aa418bfa023f1f1986b9473f536373
Matthew Currier <m.currier@afglobalcorp.com>
2016-11-01 11:45:09 -0500

    Timestamp filename

--- log.cmd
+++ log.cmd
@@ -1,7 +1,4 @@
-set filename=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
+set filename=ICS_%DATE:~-4%_%DATE:~4,2%_%DATE:~7,2%_%TIME:~0,2%_%TIME:~3,2%.dat
+set filename=%filename: =0%
 
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
-echo 
-
-node plc_logger.js DAQ.json > DAQ_%mydate%_%mytime%.dat
+node plc_logger.js DAQ.json > DAQ_%filename%.dat
